f509146b41eaf8cfceb3194934948db7dc7b9afe
eogkdfh <eogkdfh@gmail.com>
2021-10-12 12:54:13 +0900

    mysite

--- mysite02/src/main/java/com/douzone/mysite/controller/UserController.java
+++ mysite02/src/main/java/com/douzone/mysite/controller/UserController.java
@@ -2,25 +2,30 @@ package com.douzone.mysite.controller;
 
 import java.io.IOException;
 
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.douzone.mysite.web.utill.MvcUtil;
 
-public class MainController extends HttpServlet {
+
+public class UserController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		request.setCharacterEncoding("utf-8");
+		System.out.println("User Controller Called");
 		
-		System.out.println("Main Controller Called");
+		String action = request.getParameter("a");
+		if("joinform".equals(action)) {
+			MvcUtil.forward("/WEB-INF/views/user/joinform.jsp",request,response);
+			
+		}  else {
+			MvcUtil.redirect("/mysite02", request, response);
+		}
 		
-		//MvcUtil
-		RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/main/index.jsp");
-		rd.forward(request, response);
 		
 	}
 
